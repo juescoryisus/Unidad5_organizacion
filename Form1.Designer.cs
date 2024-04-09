ac030e9e0b7764e80f366edfe8f2255fa30802c9
juescoryisus <jesuseduardog204@gmail.com>
2024-04-08 19:14:18 -0600

    primer repositorio

---
+++ Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Organizacioness
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCargarCarpeta = new System.Windows.Forms.Button();
+            this.btnOrdenar = new System.Windows.Forms.Button();
+            this.btnOrdenar_por_fecha = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnCargarCarpeta
+            // 
+            this.btnCargarCarpeta.Location = new System.Drawing.Point(25, 24);
+            this.btnCargarCarpeta.Name = "btnCargarCarpeta";
+            this.btnCargarCarpeta.Size = new System.Drawing.Size(135, 54);
+            this.btnCargarCarpeta.TabIndex = 0;
+            this.btnCargarCarpeta.Text = "Cargar Carpeta";
+            this.btnCargarCarpeta.UseVisualStyleBackColor = true;
+            this.btnCargarCarpeta.Click += new System.EventHandler(this.btnCargarCarpeta_Click_1);
+            // 
+            // btnOrdenar
+            // 
+            this.btnOrdenar.Location = new System.Drawing.Point(176, 24);
+            this.btnOrdenar.Name = "btnOrdenar";
+            this.btnOrdenar.Size = new System.Drawing.Size(135, 54);
+            this.btnOrdenar.TabIndex = 1;
+            this.btnOrdenar.Text = "Ordenar";
+            this.btnOrdenar.UseVisualStyleBackColor = true;
+            this.btnOrdenar.Click += new System.EventHandler(this.btnOrdenar_Click_1);
+            // 
+            // btnOrdenar_por_fecha
+            // 
+            this.btnOrdenar_por_fecha.Location = new System.Drawing.Point(327, 24);
+            this.btnOrdenar_por_fecha.Name = "btnOrdenar_por_fecha";
+            this.btnOrdenar_por_fecha.Size = new System.Drawing.Size(135, 54);
+            this.btnOrdenar_por_fecha.TabIndex = 2;
+            this.btnOrdenar_por_fecha.Text = "ordenar por fecha";
+            this.btnOrdenar_por_fecha.UseVisualStyleBackColor = true;
+            this.btnOrdenar_por_fecha.Click += new System.EventHandler(this.btnOrdenar_por_fecha_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(25, 123);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(1210, 344);
+            this.listBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1295, 556);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnOrdenar_por_fecha);
+            this.Controls.Add(this.btnOrdenar);
+            this.Controls.Add(this.btnCargarCarpeta);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCargarCarpeta;
+        private System.Windows.Forms.Button btnOrdenar;
+        private System.Windows.Forms.Button btnOrdenar_por_fecha;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,105 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.ComponentModel;
+//using System.Data;
+//using System.Drawing;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using System.Windows.Forms;
+
+//namespace Organizacioness
+//{
+//    public partial class Form1 : Form
+//    {
+//        public Form1()
+//        {
+//            InitializeComponent();
+//        }
+
+//        private void Form1_Load(object sender, EventArgs e)
+//        {
+
+//        }
+//    }
+//}
+using System;
+using System.IO;
+using System.Windows.Forms;
+using System.Linq;
+
+namespace Organizacioness
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCargarCarpeta_Click_1(object sender, EventArgs e)
+        {
+            using (var folderDialog = new FolderBrowserDialog())
+            {
+                folderDialog.Description = "Selecciona las carpetas";
+                folderDialog.RootFolder = Environment.SpecialFolder.Desktop;
+                folderDialog.ShowNewFolderButton = false;
+                folderDialog.SelectedPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+                if (folderDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string[] selectedFolders = folderDialog.SelectedPath.Split(';');
+                    CargarArchivosEnLista(selectedFolders);
+                }
+            }
+        }
+
+        private void CargarArchivosEnLista(string[]folders)
+        {
+            listBox1.Items.Clear();
+
+            try
+            {
+                foreach (var folder in folders)
+                {
+                    var files = Directory.GetFiles(folder);
+                    foreach (var file in files)
+                    {
+                        listBox1.Items.Add(file);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al cargar archivos: " + ex.Message);
+            }
+        }
+
+        private void btnOrdenar_Click_1(object sender, EventArgs e)
+        {
+            if (listBox1.Items.Count > 0)
+            {
+                listBox1.Sorted = true;
+            }
+            else
+            {
+                MessageBox.Show("Primero carga una carpeta para ordenar los archivos.");
+            }
+        }
+
+        private void btnOrdenar_por_fecha_Click(object sender, EventArgs e)
+        {
+            if (listBox1.Items.Count > 0)
+            {
+                var files = listBox1.Items.Cast<string>().OrderBy(f => File.GetCreationTime(f)).ToArray();
+                listBox1.Items.Clear();
+                listBox1.Items.AddRange(files);
+            }
+            else
+            {
+                MessageBox.Show("Primero carga una carpeta para ordenar los archivos.");
+            }
+        }
+
+    }
+}
